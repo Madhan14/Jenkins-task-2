435d01cf2faa5d59c1cea78c0131f4166af71ecf
root <root@ip-172-31-43-240.ap-south-1.compute.internal>
2025-08-25 19:10:47 +0000

    Finel Update

--- hello.sh
+++ hello.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
 echo "Hello from Jenkins Pipeline! Build was successful."
+echo "Current date and time: $(date)"
+echo "Current user: $(whoami)"
+echo "Uptime of the system: $(uptime)"
+echo "Disk usage: $(df -h | grep '/$')"
